e0ba77020591dca5ac38140315250cba37603ede
Paul Iogna <paul.iogna@gmail.com>
2016-02-20 22:47:21 -0500

    modifies seed.rb to use json file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'json'
+
+seed_file = File.read("db/bars.json")
+json = JSON.parse(seed_file)
+json.each {|bar| Bar.create(bar)}
