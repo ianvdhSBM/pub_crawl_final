59ce1e84808614f05ad1f10dc3bbbe858c56191c
Paul Iogna <paul.iogna@gmail.com>
2016-02-22 10:57:30 -0500

    Updates the seed.rb file to create a crawl from the bar data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,14 @@ require 'json'
 seed_file = File.read("db/bars.json")
 json = JSON.parse(seed_file)
 json.each {|bar| Bar.create(bar)}
+bars = []
+bars << Bar.find_by(name: "Horseshoe Tavern")
+bars << Bar.find_by(name: "The Cameron House")
+bars << Bar.find_by(name: "Nocturne")
+bars << Bar.find_by(name: "Tattoo Queen West")
+
+crawl = Crawl.new(name: "Cool Crawl", description: "A cool crawl to go on.")
+crawl.save
+bars.each_with_index do |bar,index|
+  crawl.hops.create(bar: bar, position: index + 1)
+end
