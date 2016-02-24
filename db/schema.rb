0a71f0cdd53192bdb34875182edd8a876d6a181b
Ian Van Den Heuvel <ianvdh@gmail.com>
2016-02-23 19:40:08 -0500

    New migration to add fname, lname and image to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160221200451) do
+ActiveRecord::Schema.define(version: 20160224000427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +106,9 @@ ActiveRecord::Schema.define(version: 20160221200451) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
