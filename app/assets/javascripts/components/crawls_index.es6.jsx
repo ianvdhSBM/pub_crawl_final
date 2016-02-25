e02b4ae3c0d04a294075042fe7ee1e704488670e
Paul Iogna <paul.iogna@gmail.com>
2016-02-25 17:47:35 -0500

    almost there

--- app/assets/javascripts/components/crawls_index.es6.jsx
+++ app/assets/javascripts/components/crawls_index.es6.jsx
@@ -3,25 +3,50 @@ class CrawlsIndex extends React.Component {
   constructor(props) {
     super();
     this.setExpanded = this.setExpanded.bind(this);
+    var markers = [];
+    for(var i = 0; i < props.crawls.length; i++){
+      markers.push({
+        lat: props.crawls[i].hops[0].lat,
+        lng: props.crawls[i].hops[0].lng,
+        title: props.crawls[i].hops[0].name
+      });
+    }
     this.state = {
       crawls: props.crawls,
       expanded: null,
       mapCoordinates: {
         lat: 43.645425,
         lng: -79.395020
-      }
+      },
+      markers: markers
     }
   }
 
   setExpanded(id) {
-    this.setState({expanded: id})
+
+    var crawl = this.state.crawls.filter(function(c){
+      return c.id === id;
+    });
+    var markers = []
+    debugger
+    for(var i = 0; i < crawl.hops.length; i++){
+      markers.push({
+        lat: crawl.hops[i].lat,
+        lng: crawl.hops[i].lng,
+        title: crawl.hops[i].name
+      });
+    }
+    this.setState({
+      expanded: id,
+      markers: markers
+    });
   }
 
   render () {
     var self = this;
     return (
       <div className="container page-wrapper">
-        <Map lat={this.state.mapCoordinates.lat} lng={this.state.mapCoordinates.lng}/>
+        <Map lat={this.state.mapCoordinates.lat} lng={this.state.mapCoordinates.lng} markers={this.state.markers}/>
         <Description />
         <h1>hops.</h1>
           <CrawlsList crawls={ this.state.crawls } setExpanded={ self.setExpanded } expanded={ this.state.expanded }/>
