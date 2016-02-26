45bf296bc900c926a6d442ce47266b88338b6ced
Ian Van Den Heuvel <ianvdh@gmail.com>
2016-02-26 12:55:43 -0500

    Update setExpanded method to reset map when crawlDetails component is closed

--- app/assets/javascripts/components/crawls_index.es6.jsx
+++ app/assets/javascripts/components/crawls_index.es6.jsx
@@ -3,14 +3,16 @@ class CrawlsIndex extends React.Component {
   constructor(props) {
     super();
     this.setExpanded = this.setExpanded.bind(this);
-    var markers = [];
-    for(var i = 0; i < props.crawls.length; i++){
-      markers.push({
-        lat: props.crawls[i].hops[0].lat,
-        lng: props.crawls[i].hops[0].lng,
-        title: props.crawls[i].name
-      });
-    }
+
+    var markers = this.setDefaultMarkers(props.crawls)
+    // for(var i = 0; i < props.crawls.length; i++){
+    //   markers.push({
+    //     lat: props.crawls[i].hops[0].lat,
+    //     lng: props.crawls[i].hops[0].lng,
+    //     title: props.crawls[i].name
+    //   });
+    // }
+
     this.state = {
       crawls: props.crawls,
       expanded: null,
@@ -22,22 +24,44 @@ class CrawlsIndex extends React.Component {
     }
   }
 
-  setExpanded(id) {
-    var crawl = this.state.crawls.filter(function(c){
-      return c.id === id;
-    });
+  setDefaultMarkers(crawls) {
+    // var crawls = this.props.crawls;
     var markers = [];
-    for(var i = 0; i < crawl[0].hops.length; i++){
+    for(var i = 0; i < crawls.length; i++){
       markers.push({
-        lat: crawl[0].hops[i].lat,
-        lng: crawl[0].hops[i].lng,
-        title: crawl[0].hops[i].name
+        lat: crawls[i].hops[0].lat,
+        lng: crawls[i].hops[0].lng,
+        title: crawls[i].name
       });
     }
-    this.setState({
-      expanded: id,
-      markers: markers
-    });
+    return markers;
+  }
+
+  setExpanded(id) {
+    if (id === null) {
+      var markers = this.setDefaultMarkers(this.state.crawls);
+      this.setState({
+        expanded: id,
+        markers: markers
+      });
+    } else {
+      var crawl = this.state.crawls.filter(function(c){
+        return c.id === id;
+      });
+      var markers = [];
+      for(var i = 0; i < crawl[0].hops.length; i++){
+        markers.push({
+          lat: crawl[0].hops[i].lat,
+          lng: crawl[0].hops[i].lng,
+          title: crawl[0].hops[i].name
+        });
+      }
+
+      this.setState({
+        expanded: id,
+        markers: markers
+      });
+    }
   }
 
   render () {
