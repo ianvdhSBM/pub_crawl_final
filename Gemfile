6c47ed62fe747d71f5bf46e9c9a80a4865914d4d
Hanna Jones <jones.hanna.m@gmail.com>
2016-02-29 14:36:52 -0500

    facebook oauth mostly working - just redirects too quickly

--- Gemfile
+++ Gemfile
@@ -41,7 +41,10 @@ gem 'font-awesome-sass'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+# debugging
 gem 'pry'
+
+gem 'omniauth-facebook'
 # Use Unicorn as the app server
 # gem 'unicorn'
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,6 +239,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, "1768705696686469
+", "cacc0c46e7e89e29bac64bf1df3c8e16"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
