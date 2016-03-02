32045aae0d1fcaeb0a6c91521ccbfab056a298a2
Hanna Jones <jones.hanna.m@gmail.com>
2016-03-02 16:34:41 -0500

    bar show page aligned left column

--- app/assets/javascripts/components/bar_show.es6.jsx
+++ app/assets/javascripts/components/bar_show.es6.jsx
@@ -50,18 +50,18 @@ class BarShow extends React.Component {
       <div id="flexbox-container-columns">
         <div id="crawls-container-flex">
           <div className="crawlList-flex">
-            <div className="crawl-name">
+            <div className="bar-details">
               <h2>{bar.name}</h2>
+              <p><strong>Website: </strong><a target="_blank" href={bar.website}>{bar.website}</a></p>
+              <p><strong>Address: </strong>{bar.address}, {bar.city}, {bar.province}</p>
+              <p><strong>Phone: </strong>{ bar.phone_number ? bar.phone_number : <em>Not listed</em>}</p>
+              {/* <p><strong>Average Price: </strong>{bar.price}</p> */}
             </div>
-            <p><strong>Website: </strong><a target="_blank" href={bar.website}>{bar.website}</a></p>
-            <p><strong>Address: </strong>{bar.address}, {bar.city}, {bar.province}</p>
-            <p><strong>Phone: </strong>{ bar.phone_number ? bar.phone_number : <em>Not listed</em>}</p>
-            {/* <p><strong>Average Price: </strong>{bar.price}</p> */}
+            <div>
+              <h3>{bar.name}'s hop list</h3>
+                <CrawlsList crawls={ crawls } tags={ this.props.tags } setExpanded={ self.setExpanded } expanded={ this.state.expanded }/>
+            </div>
           </div>
-        </div>
-        <div>
-          <h3>{bar.name}'s hop list</h3>
-            <CrawlsList crawls={ crawls } tags={ this.props.tags } setExpanded={ self.setExpanded } expanded={ this.state.expanded }/>
         </div>
         <div id="map-flex">
           <Map lat={this.state.mapCoordinates.lat} lng={this.state.mapCoordinates.lng} markers={ this.state.markers }/>
